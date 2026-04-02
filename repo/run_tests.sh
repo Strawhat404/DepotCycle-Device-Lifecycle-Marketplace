e40648c3885944e9d80df3f617435af0e272868c
strawhat404 <Yosephtesfaye27@gmail.com>
2026-04-02 21:12:07 +0300

    Fix: remove source cargo/env in docker fallback, cargo is already in PATH in rust image

--- repo/run_tests.sh
+++ repo/run_tests.sh
@@ -20,5 +20,5 @@ else
     -v "$(pwd):/work" \
     -w /work \
     rust:1.77 \
-    bash -c 'source $HOME/.cargo/env && cargo test -p backend && cargo test -p unit_tests && cargo test -p API_tests'
+    bash -c 'cargo test -p backend && cargo test -p unit_tests && cargo test -p API_tests'
 fi
